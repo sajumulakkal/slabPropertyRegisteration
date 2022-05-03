3e04508ab54c7a46751ad17154a25af352623914
Saju Mulakkal <sajumulakkal@hotmail.com>
2022-05-03 12:51:52 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends Component {
   async loadBlockchainData() {
     const web3 = new Web3(Web3.givenProvider || "http://localhost:7545")
     const accounts = await web3.eth.getAccounts()
+    const networks = await web3.eth.net.getNetworkType()
     this.setState({ account: accounts[0] })
+    this.setState({ network: networks })
     const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS)
     this.setState({ todoList })
     const taskCount = await todoList.methods.taskCount().call()
@@ -58,8 +60,10 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1>Hello, World!</h1>
+        <p>Your account: {this.state.account} and connected to network: {this.state.network}</p>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="http://karmacoins.info" target="_blank" rel = "noopener noreferrer">sLabs| Property Registeration ... once Property added- To change ownership, you need to sell the property...</a>
+          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="http://karmacoins.info" target="_blank" rel = "noopener noreferrer">sLabs| Property Registeration ... once Property added- To change ownership, you need to sell the property...{this.state.network}</a>
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
               <small><a className="nav-link"  href="."><span id="account"></span></a></small>
